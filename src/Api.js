32b49152387d046c22bd9da437214683014fbceb
NeKz <nekzor@users.noreply.github.com>
2019-09-30 22:22:55 +0200

    Remove dev code in api client

--- src/Api.js
+++ src/Api.js
@@ -1,11 +1,5 @@
 class Api {
     async request(game, date) {
-        if (process.env.NODE_ENV === 'development') {
-            try {
-                return (await import(`./api/${game}/${date || 'latest'}.json`)).default;
-            } catch (err) {}
-            return [];
-        }
         let res = await fetch(`https://raw.githubusercontent.com/NeKzor/tmx-records/api/${game}/${date || 'latest'}.json`);
         console.log(`GET ${res.url} (${res.status})`);
         return res.ok ? await res.json() : [];
