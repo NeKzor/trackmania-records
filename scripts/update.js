765e8c2ff72fc05a9318824c17f3ecef9974b88b
NeKz <nekzor@users.noreply.github.com>
2021-05-16 20:02:12 +0200

    Adjust update inteveral

--- scripts/update.js
+++ scripts/update.js
@@ -90,5 +90,7 @@ if (nowTrackmania) {
 }
 
 cron.schedule('0 19 * * *', main);
-cron.schedule('5,10,15,20,25,30,35,45,50,55 19 * * *', trackmaniaOnly);
-cron.schedule('0,5,10,15,20,25,30,35,45,50,55 0-18,20-23 * * *', trackmaniaOnly);
+//cron.schedule('5,10,15,20,25,30,35,45,50,55 19 * * *', trackmaniaOnly);
+//cron.schedule('0,5,10,15,20,25,30,35,45,50,55 0-18,20-23 * * *', trackmaniaOnly);
+cron.schedule('15,30,45 19 * * *', trackmaniaOnly);
+cron.schedule('0,15,30,45 0-18,20-23 * * *', trackmaniaOnly);
